1f3a04d154302020d111213d5476bb0de238ce3d
Kacper Stankiewicz <kstankiewicz@lbitai2.knbit.edu.pl>
2026-04-28 23:31:51 +0200

    save progress

--- src/datasets/models.py
+++ src/datasets/models.py
@@ -4,17 +4,24 @@ import torch
 
 
 class ProteinLigandDataset(Dataset):
-    def __init__(self, df: pl.DataFrame, smiles_map: dict):
-        self.smiles = df["Ligand SMILES"].to_list()
+    def __init__(self, df, smiles_map):
         self.sequences = df["Tokenized_Sequence"].to_list()
-        self.labels = df["is_active"].to_list()
-        self.smiles_map = smiles_map
+        self.labels    = df["is_active"].to_list()
+        self.smiles    = df["Ligand SMILES"].to_list()
+
+        self.graph_store = {}
+        for smi, data in smiles_map.items():
+            self.graph_store[smi] = {
+                "x":          data.x,
+                "edge_index": data.edge_index,
+                "edge_attr":  data.edge_attr,
+            }
+
+    def __getitem__(self, idx):
+        g   = self.graph_store[self.smiles[idx]]
+        seq = torch.tensor(self.sequences[idx], dtype=torch.long)
+        y   = torch.tensor(self.labels[idx],    dtype=torch.float)
+        return g, seq, y
 
     def __len__(self):
         return len(self.labels)
-
-    def __getitem__(self, idx):
-        graph = self.smiles_map[self.smiles[idx]].clone()
-        seq = torch.tensor(self.sequences[idx], dtype=torch.long)
-        y = torch.tensor(self.labels[idx], dtype=torch.float)
-        return graph, seq, y
